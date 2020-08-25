26f6ec591d04de147de2bdc25111fe31aab257d5
luwei-zk <349012093@qq.com>
2020-08-25 23:20:22 +0800

    Add tabbar and Config scss

--- pages/tabbar/my/my.vue
+++ pages/tabbar/my/my.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		首页
+		我的
 	</view>
 </template>
 
@@ -8,17 +8,15 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			
 		}
 	}
 </script>
 
 <style>
+
 </style>
