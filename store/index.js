b1a143fbc58d786380d463f90e532c2f8dd36fce
wb-zk766067 <wb-zk766067@alibaba-inc.com>
2020-09-04 13:44:34 +0800

    Set history persistent

--- store/index.js
+++ store/index.js
@@ -9,7 +9,9 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
 	// 数据源
 	state: {
-		historyLists: []
+		// historyLists: []
+		// 从本地缓存中去读，不存在返回空数组
+		historyLists: uni.getStorageSync("__history") || []
 	},
 	// 修改数据源的值
 	mutations: {
@@ -25,16 +27,18 @@ const store = new Vuex.Store({
 	actions: {
 		// 添加历史记录 history 从客户端传进来的
 		// 页面调用 此方法，该方法的commit调用SET_HISTORY_LISTS修改historyLists
-		set_history({
-			commit,
-			state
-		}, history) {
+		set_history({commit,state}, history) {
 			let list = state.historyLists
 			// 数组前面插入值
 			list.unshift(history)
+			// 将最新的 list 设置为缓存
+			uni.setStorageSync('__history', list)
 			commit('SET_HISTORY_LISTS', list)
 		},
 		clearHistory({commit}) {
+			// 将本地缓存置空
+			// uni.setStorageSync('__history', []) 这个也行
+			uni.removeStorageSync('__history')
 			commit('CLEAR_HISTORY_LISTS')
 		}
 	}
